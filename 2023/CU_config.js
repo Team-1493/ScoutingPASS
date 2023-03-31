30b40ac2f042719c0eda86599899ecf1387e3bca
Team-1493 <falcons1493@gmail.com>
2023-03-31 09:23:08 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2020ilch",
+      "defaultValue": "2023nytr",
       "required": "true",
       "disabled": "true"
     },
